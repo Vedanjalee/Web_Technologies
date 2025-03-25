c414ff2f438b72de09a225dfaa2569afb5828b27
Anjali Yadav <anjaliyadavp1@gmail.com>
2025-03-25 23:53:45 +0530

    "Toogle"

---
+++ DOM_Akshay_Sir/toogle.js
@@ -0,0 +1,10 @@
+// ELEMENT,classList 
+
+// let btn = document.querySelector('button');
+// let body = document.body;
+// btn.addEventListener('click', () => {
+//     body.classList.add("dark")
+// })
+
+let wheelbtn = document.querySelector('#wb')
+let wc= document.querySelector
